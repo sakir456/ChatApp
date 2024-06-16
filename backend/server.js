76bbbe1802a95bef163f3e7e2cdd946b76c0558f
sakir456 <shaikhsakir1015@gmail.com>
2024-06-16 18:59:49 +0530

    Backend and UI design

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+dotenv.config();
+
+app.use(express.json()); //to parse the incoming requests with JSON payloads(from req.body) 
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
+
+// app.get("/", (req, res) => {
+//     res.send("Hello world!")
+// });
+
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`server is running on port ${PORT}`)
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
+import Home from './pages/home/Home'
+
+
+
+
+
+
+
+
+
+
+
+function App() {
+ 
+
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+    <Home /> 
+    </div>
+  )
+}
+
+export default App

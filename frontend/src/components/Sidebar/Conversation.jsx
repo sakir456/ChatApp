8bb3e114e1e23a0f9e071ef6a05fc67b2386a517
sakir456 <shaikhsakir1015@gmail.com>
2024-06-18 00:03:42 +0530

    realtime names fetched from api and showcase to conversations

--- frontend/src/components/Sidebar/Conversation.jsx
+++ frontend/src/components/Sidebar/Conversation.jsx
@@ -1,26 +1,26 @@
 
 
-const Conversation = () => {
+const Conversation = ({conversation,lastIdx,emoji}) => {
   return (
     <div >
         <div className="flex  gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
            <div className="avatar online">
            <div className="w-12 rouded-full">
-            <img src="/avatar.png" 
+            <img src={conversation.profilePic}
             alt="user avatar" />
            </div>
 
            </div>
            <div className="flex flex-col flex-1">
            <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">Sakir Shaikh</p>
-            <span className="text-xl">😒</span>
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
             </div>
            </div>
         </div>
-        <div className="divider my-0 py-0 h-1">
+        {!lastIdx && <div className="divider my-0 py-0 h-1"/>}
 
-        </div>
+       
         <div>
 
         </div>
